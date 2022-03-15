30aa4087fefe64bf01ce0d61283c1d0ae5079b48
NAYCKERSON PEDRO <99208486+CROWNIKE@users.noreply.github.com>
2022-03-15 16:13:33 -0300

    Adicionar arquivos de projeto.

---
+++ BBULLS JEANS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BBULLS_JEANS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
